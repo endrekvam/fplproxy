2a32651e8bd156ad000e094a5309efff867f1638
Endre Kvam <endre.kvam@hotmail.com>
2024-12-13 06:18:11 +0100

    tester med cors

--- api/bootstrap-static.js
+++ api/bootstrap-static.js
@@ -1,15 +1,22 @@
 import fetch from 'node-fetch';
+import Cors from 'cors';
+
+const cors = Cors({
+    methods: ['GET'],
+    origin: 'https://endrekvam.github.io/leaguetrackerfpl/'
+})
 
 // Info om lag, kamper og gameweeks
 
 export default async (req, res) => {
-
-  try {
-    const response = await fetch('https://fantasy.premierleague.com/api/bootstrap-static/');
-    const data = await response.json();
-    res.status(200).json(data);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Failed to fetch data' });
-  }
+    cors(req, res, async () => {
+        try {
+            const response = await fetch('https://fantasy.premierleague.com/api/bootstrap-static/');
+            const data = await response.json();
+            res.status(200).json(data);
+          } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Failed to fetch data' });
+          }
+    });
 };
